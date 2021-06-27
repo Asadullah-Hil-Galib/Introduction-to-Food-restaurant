1c0078cbfe2c97b76aed1b1818c49a22624d5c06
Asadullah-Hil-Galib <86350811+Asadullah-Hil-Galib@users.noreply.github.com>
2021-06-27 04:12:22 -0600

    Add files via upload

---
+++ js/my.js
@@ -0,0 +1,18 @@
+let counters = document.querySelectorAll('.counter')
+let time = 800
+counters.forEach(counter => {
+    let updateCount = () => {
+        let target = +counter.getAttribute('data-target')
+        let count = +counter.innerText
+
+        let increment = target / time
+
+        if(count < target) {
+            counter.innerText = Math.ceil(count + increment)
+            setTimeout(updateCount, 1)
+        } else {
+            count.innerText = target
+        }
+    }
+    updateCount()
+})
